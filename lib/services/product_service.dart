f2e6bdf01b82857e38afd806a629a1db66e023fb
chheng <oeungchungchheng2205@gmail.com>
2025-03-05 14:52:56 +0700

    Close #8 create cache store data from api

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,13 +1,33 @@
 import 'dart:convert';
+import 'package:ecomerce_app/constants/api_constants.dart';
 import 'package:ecomerce_app/models/Product.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProductService {
-  final String apiUrl = 'https://dummyjson.com/products';
+  final String apiUrl = ApiConstants.productApi;
 
   Future<List<Product>> fetchProducts() async {
     try {
-      await Future.delayed(Duration(seconds: 2));
+      final pref = await SharedPreferences.getInstance();
+      String? cacheData = pref.getString('cache_product');
+      int? lastFetchTime = pref.getInt('last_fetch_time');
+
+      // Check if cached data exists and is still valid
+      if (cacheData != null && lastFetchTime != null){
+        DateTime lastFetched = DateTime.fromMillisecondsSinceEpoch(lastFetchTime);
+        DateTime now = DateTime.now();
+
+        // Check if cache is older than 1 hour
+        if (now.difference(lastFetched).inHours < 1) {
+          final List<dynamic> cachedJson = json.decode(cacheData);
+
+          List<Product> cacheProducts =
+              cachedJson.map((json) => Product.fromJson(json)).toList();
+
+          return cacheProducts;
+        }
+      }
 
       final response = await http.get(Uri.parse('${apiUrl}?limit=5'));
 
@@ -16,7 +36,12 @@ class ProductService {
 
         List<dynamic> productsJson = data['products'];
 
-        List<Product> products = productsJson.map((json) => Product.fromJson(json)).toList();
+        List<Product> products =
+            productsJson.map((json) => Product.fromJson(json)).toList();
+
+        // Cache the fetched products and store the current time
+        await pref.setString('cache_product', json.encode(productsJson));
+        await pref.setInt('last_fetch_time', DateTime.now().millisecondsSinceEpoch);
 
         return products;
       } else {
@@ -26,29 +51,58 @@ class ProductService {
       throw Exception('Error: $e');
     }
   }
-
+  
   Future<List<Product>> fetchProductByCreatedAt({ int limit = 10}) async {
-    await Future.delayed(Duration(seconds: 2));
+    final pref = await SharedPreferences.getInstance();
+    String? cacheData = pref.getString('cache_prodcut_created_at');
+    int? lastFetchTime = pref.getInt('last_fetch_time_created_at');
 
-    final response = await http.get(Uri.parse('${apiUrl}?limit=${limit}'));
+    if (cacheData != null && lastFetchTime != null) {
+      DateTime lastFetched = DateTime.fromMillisecondsSinceEpoch(lastFetchTime);
+      DateTime now = DateTime.now();
 
-    try {
-      if(response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        List<dynamic> productsJson = data['products'];
+      if(now.difference(lastFetched).inHours < 1) {
 
-         List<Product> products =
-            productsJson.map((json) => Product.fromJson(json)).toList();
+        final List<dynamic> cachedJson = json.decode(cacheData);
+        List<Product> cachedProducts = cachedJson.map((json) => Product.fromJson(json)).toList();
 
-        products.sort((a, b) => b.createdAt.compareTo(a.createdAt)); 
-
-        return products.take(limit).toList();
-      } else{
-        throw Exception("Failed to load products");
+        return cachedProducts.take(limit).toList();
       }
-    } catch(error){
-      throw Exception("Failed to load products: ${error}");
     }
 
+    try{
+       final response = await http.get(Uri.parse('${apiUrl}?limit=${limit}'));
+       
+       if(response.statusCode == 200) {
+        final data = json.decode(response.body);
+        List<dynamic> productsJson = data['products'];
+
+        List<Product> products = productsJson.map((json) => Product.fromJson(json)).toList();
+
+        products.sort((a,b) => b.createdAt.compareTo(a.createdAt));
+
+        await pref.setString('cache_product_created_at', json.encode(productsJson));
+        await pref.setInt('last_fetch_time_created_at', DateTime.now().millisecondsSinceEpoch);
+
+        return products.take(limit).toList();
+       } else {
+        throw Exception("Failed to load products22");
+       }
+    } catch(error){
+      throw Exception("Failed to load products: $error");
+    }
+  }
+
+  // Method to clear cache
+  Future<void> clearCache() async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.remove('cache_product');
+    await pref.remove('last_fetch_time');
+  }
+
+   Future<void> clearCacheFetchProductCreatedAt() async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.remove('cache_product_created_at');
+    await pref.remove('last_fetch_time_created_at');
   }
 }
--- lib/widgets/home_screen/new_section_widget.dart
+++ lib/widgets/home_screen/new_section_widget.dart
@@ -6,10 +6,30 @@ import 'package:ecomerce_app/widgets/header_section_widget.dart';
 import 'package:ecomerce_app/widgets/skeleton_loader_widget.dart';
 import 'package:flutter/material.dart';
 
-class NewSectionWidget extends StatelessWidget {
+class NewSectionWidget extends StatefulWidget {
   NewSectionWidget({super.key});
 
-  final ProductController _controller = ProductController();
+  @override
+  State<NewSectionWidget> createState() => _NewSectionWidgetState();
+}
+
+class _NewSectionWidgetState extends State<NewSectionWidget> {
+  final ProductController _productController = ProductController();
+  late Future<List<Product>> _productFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _productFuture = _productController.filterProductByCreatedAt();
+  }
+
+  Future<void> _refreshData() async {
+    await _productController.clearCacheFetchByCreatedAt(() {
+      setState(() {
+        _productFuture = _productController.filterProductByCreatedAt();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +48,17 @@ class NewSectionWidget extends StatelessWidget {
             width: double.infinity,
             height: 300,
             child: FutureBuilder<List<Product>>(
-              future: _controller.filterProductByCreatedAt(),
+              future: _productFuture,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 5, 
-                    itemBuilder: (context, index) => SkeletonLoaderWidget(),
+                  return RefreshIndicator(
+                    onRefresh: _refreshData,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5, 
+                      itemBuilder: (context, index) => SkeletonLoaderWidget(),
+                    ),
                   );
                 } else if (snapshot.hasError) {
                   return Center(child: Text("Error: ${snapshot.error}"));
